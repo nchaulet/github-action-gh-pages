65d045c0f1aee9a700711437ea805779daa464fd
Dongwoon Son <dplusic@gmail.com>
2018-12-30 09:44:42 +0900

    Exit script on error

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,12 +5,18 @@ if [ ! -n "$PUBLIC_PATH" ]; then
 fi
 if [ ! -n "$GITHUB_TOKEN" ]; then
   echo "You need to supply GITHUB_TOKEN"
+  exit 1
 fi
 
 git config user.name "${GITHUB_ACTOR}"
 git config user.email "${GITHUB_ACTOR}@users.noreply.github.com"
 
 gh-pages -d $PUBLIC_PATH -b gh-pages
+retval=$?
+if [ $retval -ne 0 ]; then
+  echo "gh-pages failed: $retval"
+  exit 2
+fi
 
 curl --request POST -H "Authorization: token ${GITHUB_TOKEN}" \
   --url "https://api.github.com/repos/$GITHUB_REPOSITORY/pages/builds" \
